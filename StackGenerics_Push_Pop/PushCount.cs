6b9007fbb1c71a84a5640ef6ce5f2d4e1e18906e
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-11-04 16:52:20 +0200

    Add project files.

---
+++ StackGenerics_Push_Pop/PushCount.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var MyStack = new Stack<char>();
+            MyStack.Push('A');
+            MyStack.Push('N');
+            MyStack.Push('X');
+
+            Console.WriteLine("Исходный стек: ");
+            foreach (char s in MyStack)
+                Console.Write(s);
+            Console.WriteLine("\n");
+
+            while (MyStack.Count > 0)
+            {
+                Console.WriteLine("Pop -> {0}", MyStack.Pop());
+            }
+
+            if (MyStack.Count == 0)
+                Console.WriteLine("\nСтек пуст!");
+
+        }
+    }
+}
